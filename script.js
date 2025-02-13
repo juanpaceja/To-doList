28b09f1f01b8d7f7a0453225970f9bfb3ea85fee
juanpaceja <143227440+juanpaceja@users.noreply.github.com>
2025-02-12 20:06:44 -0600

    update: added comments in the code to make it more understandable

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const inputbox = document.getElementById("Input-box");
 const listContainer = document.getElementById("list-container");
+const ButtonContainer = document.getElementById("Delete-all");
 
+
+//function to add a task//
 function addTask(){
     if(inputbox.value === ''){
         alert("you must write something!");
-    }
-    else{
+    } else{
         let li = document.createElement("li");
         li.innerHTML = inputbox.value;
         listContainer.appendChild(li);
+
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
+        span.classList.add("delete-task");
         li.appendChild(span);
     }
     inputbox.value = "";
     saveData();
 }
 
+//function to mark as checked the tasks//
 listContainer.addEventListener("click", function (e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
@@ -25,17 +30,21 @@ listContainer.addEventListener("click", function (e){
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         saveData();
+        checkDeleteAllButton();
     }
 },false );
+
+//Local storage functions//
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
 }
 
 function showtask(){
-    listContainer.innerHTML = localStorage.getItem("data");
+    listContainer.innerHTML = localStorage.getItem("data") || "";
 }
 showtask();
 
+//Dinamic placeholders//
 const placeholders = ["Do laundry", "walk the dog", "Buy groceries", "change oil"];
 let index = 0;
 const input = document.getElementById("Input-box");
@@ -53,6 +62,7 @@ function changePlaceholder() {
 
 setInterval(changePlaceholder, 2000)
 
+//Add task when enter key is pulsed, when the input is selected//
 document.getElementById("Input-box")
     .addEventListener("keyup", function(event) {
     event.preventDefault();
